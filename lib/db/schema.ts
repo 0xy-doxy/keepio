3a1b5dfc1fd2c38ad11ff46d578abdfac01d78a1
pratik <hopelesshackerman007@gmail.com>
2025-10-25 12:15:52 +0530

    schemas done , zod done

---
+++ lib/db/schema.ts
@@ -0,0 +1,41 @@
+import {pgTable, uuid, text, integer, boolean, timestamp} from "drizzle-orm/pg-core"
+import {relations} from "drizzle-orm"
+
+export const files = pgTable("files", {
+    id: uuid("id").defaultRandom().primaryKey(),
+
+    //file/folder info
+    name: text("name").notNull(),
+    path: text("path").notNull(),
+    size: integer("size").notNull(),
+    type: text("type").notNull(),
+
+    //storage info
+    fileUrl: text("file_url").notNull(),
+    thumbnailUrl: text("thumbnail_url"),
+
+    // ownership
+    userId: text("user_id").notNull(),
+    parentId: uuid("parent_id"),
+
+    // file/folder flags
+    isFolder: boolean("is_folder").default(false).notNull(),
+    isStarred: boolean("is_starred").default(false).notNull(),
+    isTrash: boolean("isTrash").default(false).notNull(),
+
+    // timestamps
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+
+})
+
+export const filesRelations = relations(files, ({one, many}) => ({
+    parent: one(files, {
+        fields: [files.parentId],
+        references: [files.id]
+    }),
+    children: many(files)
+}))
+
+export const File = typeof files.$inferSelect 
+export const newFile = typeof files.$inferInsert 
